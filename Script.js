83f85bbe9fef11a2c6ed1e97c89b43fd464bf2e1
PabloRutsatz <pablohenriquerutsatz@gmail.com>
2019-12-29 23:09:49 -0300

    Exercício 1, promise simples retornando se é maior ou menor que 18.
    Rocketseat

---
+++ Script.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+function checaIdade(idade) {
+    return new Promise(function (resolve, reject) {
+        if (idade > 18) {
+            resolve(null);
+        }
+        else {
+            reject(null);
+        }
+    });
+}
+checaIdade(19)
+    .then(function () {
+        console.log("Maior que 18");
+    })
+    .catch(function () {
+        console.log("Menor que 18");
+    });
