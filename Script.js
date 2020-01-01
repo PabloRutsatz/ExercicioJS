64835ef9cbf00de121f3593d4a7d1c798172dafc
PabloRutsatz <pablohenriquerutsatz@gmail.com>
2019-12-31 23:26:18 -0300

    Rocketseat Exercício 2

--- Script.js
+++ Script.js
@@ -1,18 +1,42 @@
 // JavaScript source code
-function checaIdade(idade) {
-    return new Promise(function (resolve, reject) {
-        if (idade > 18) {
-            resolve(null);
-        }
-        else {
-            reject(null);
-        }
-    });
+
+var ul = document.querySelector('#app ul');
+var inputAdicionar = document.querySelector('#app input');
+var buttonAdicionar = document.querySelector('#app button');
+
+function RenderizaGits(listaGit) {
+
+    ul.innerHTML = '';
+
+    for (git of listaGit) {
+        var ElementUl = document.createElement('li');
+        var ElementText = document.createTextNode(git['name']);
+
+        ElementUl.appendChild(ElementText);
+        ul.appendChild(ElementUl);
+    }
 }
-checaIdade(19)
-    .then(function () {
-        console.log("Maior que 18");
-    })
-    .catch(function () {
-        console.log("Menor que 18");
-    });
+
+function AdicionaInfoGit() {
+    var nomeGit = inputAdicionar.value;
+
+    var conexao = new XMLHttpRequest();
+    //conexao.open('GET', ' https://api.github.com/users/pablorutsatz/repos');
+    conexao.open('GET', ' https://api.github.com/users/' + nomeGit + '/repos');
+    conexao.send(null);
+
+    conexao.onreadystatechange = function () {
+        if (conexao.readyState === 4) {
+            if (conexao.status == 200) {
+                var listaItens = JSON.parse(conexao.responseText);
+                console.log(listaItens);
+                RenderizaGits(listaItens);
+            }
+            else {
+                console.warn('Erro');
+            }
+        }
+    }
+}
+
+buttonAdicionar.onclick = AdicionaInfoGit;
